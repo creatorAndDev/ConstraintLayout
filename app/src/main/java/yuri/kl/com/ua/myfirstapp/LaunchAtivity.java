dcf6ed5ffb4f37ca279222fe643e8d752e5717b7
Yuri <creator.and.dev@gmail.com>
2018-02-15 19:51:02 +0200

    Initial commit

---
+++ app/src/main/java/yuri/kl/com/ua/myfirstapp/LaunchAtivity.java
@@ -0,0 +1,13 @@
+package yuri.kl.com.ua.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchAtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
